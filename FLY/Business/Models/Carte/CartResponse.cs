f80cc7e8c50fe9e5e290f12bc8f7ec3fd6247a86
tien-lai-dev <phamtannhat0@gmail.com>
2024-07-22 19:26:50 +0700

    Feature Cart

---
+++ FLY/Business/Models/Carte/CartResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace FLY.Business.Models.Carte
+{
+    public class CartResponse
+    {
+        public int CartId { get; set; }
+        public string Email { get; set; }
+        public List<ProductInCartResponse> ProductInCarts { get; set; }
+    }
+
+    public class ProductInCartResponse
+    {
+        public string ProductCategoryName { get; set; }
+        public string ProductName { get; set; }
+        public string ImageProduct { get; set; } = null!;
+        public double ProductPrice { get; set; }
+        public int CartQuantity { get; set; }
+    }
+}
---
+++ FLY/Business/Services/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using FLY.Business.Models.Carte;
+
+namespace FLY.Business.Services
+{
+    public interface ICartService
+    {
+        Task<CartResponse> GetCartOfCustomer(int accountId);
+        Task<CartResponse> AddProductToCart(CartRequest request);
+        Task<bool> SubProductToCart(SubCartRequest request);
+    }
+}

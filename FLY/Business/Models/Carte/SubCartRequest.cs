b69536c16ea7f0e933584bc0d8fa978156ed08d3
tien-lai-dev <phamtannhat0@gmail.com>
2024-07-22 16:30:19 +0700

    Feature Cart

---
+++ FLY/Business/Models/Carte/SubCartRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FLY.Business.Models.Carte
+{
+    public class SubCartRequest
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        public int AccountId { get; set; }
+    }
+}
